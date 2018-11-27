1e40b51ba5c39c204f50388e7edd4d414b2c682c
Junaid Farooq <junaid@evercam.io>
2018-11-27 18:23:26 +0500

    Added User, Posts, Profile routes.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
 const db = require("./config/keys").mongoURI;
 
 mongoose
@@ -14,6 +18,10 @@ mongoose
 const app = express();
 
 app.get("/", (req, res) => res.send("hello"));
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
