43c9db703a3209f335c26ae7a5acf6d139dbc68d
Junaid Farooq <junaid@evercam.io>
2018-11-27 08:20:30 +0500

    Connected to MongoDB through mLab.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://junaid:qwerty1@ds229909.mlab.com:29909/deviconni'
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const app = express();
+
+app.get("/", (req, res) => res.send("hello"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
