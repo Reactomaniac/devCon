778b1f42a48600e81c154d771ab52b5cfe7a2d00
Junaid Farooq <junaid@evercam.io>
2018-11-27 08:31:00 +0500

    Removed DeprecationWarning.

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(db)
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
