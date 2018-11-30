6ee774f3ab3b79327105794b5522977d69eaade6
Junaid Farooq <junaid@evercam.io>
2018-11-30 20:51:36 +0500

    Added Education, Experience with post and delete. Also delete profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -200,7 +200,52 @@ router.post("/education", passport.authenticate("jwt", {session: false}), (req, 
       profile.education.unshift(newEdu);
 
       profile.save().then(profile => res.json(profile));
-    })
+    });
+});
+
+// @route POST api/profile/experience/:exp_id
+// @desc Delete experience from Profile
+// @access Private
+router.delete("/experience/:exp_id", passport.authenticate("jwt", {session: false}), (req, res) => {
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err));
+});
+
+// @route POST api/profile/education/:edu_id
+// @desc Delete education from Profile
+// @access Private
+router.delete("/education/:edu_id", passport.authenticate("jwt", {session: false}), (req, res) => {
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err));
+});
+
+// @route POST api/profile
+// @desc Delete user and Profile
+// @access Private
+router.delete("/", passport.authenticate("jwt", {session: false}), (req, res) => {
+  Profile
+    .findOneAndRemove({ user: req.user.id })
+    .then(() => {
+      User
+        .findOneAndRemove({ _id: req.user.id })
+        .then(() =>
+          res.json({ success: true })
+          );
+    });
 });
 
 module.exports = router;
