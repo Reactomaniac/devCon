ecc21c84fc993b3556b43eb7d0df5810f25a35a4
Junaid Farooq <junaid@evercam.io>
2018-12-01 20:55:45 +0500

    Added Post GET Delete and Like routes.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,10 +4,32 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 
 const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 const validatePostInput = require("../../validation/post");
 
 router.get("/test", (req, res) => res.json({msg: "Posts Works"}));
+
+// @route GET api/posts
+// @desc Get post
+// @access Public
+router.get("/", (req, res) => {
+  Post
+    .find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post
+    .findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: "No post found with thhat ID" }));
+});
 
 // @route POST api/posts
 // @desc Create post
@@ -28,6 +50,47 @@ router.post("/", passport.authenticate("jwt", { session: false }), (req, res) =>
   });
 
   newPost.save().then(post => res.json(post));
+});
+
+// @route DELETE api/posts/:id
+// @desc Delete post
+// @access Private
+router.delete("/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
+  Profile
+    .findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ notauthorized: "User not authorized" })
+          }
+
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found"}));
+    });
+});
+
+// @route POST api/posts/like/:id
+// @desc Like a post
+// @access Private
+router.post("/like/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    console.log(req.params.id);
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.likes.filer(like => like.user.toString() === req.user.id)
+            .length > 0
+          ) {
+          return res.status(400).json({ alreadyliked: "User already liked this post" });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  });
 });
 
 module.exports = router;
