f1fb407b39b741c815263f5f82dd47874cb313a0
Junaid Farooq <junaid@evercam.io>
2018-12-15 16:22:23 +0500

    Added current user reducers.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,7 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
 import axios from "axios";
 
 // Register User
@@ -12,4 +15,31 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios.post("/api/users/login", userData)
+    .then(res => {
+      // save to localStorage
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token)
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => 
+      dispatch{(
+        type: GET_ERRORS,
+        payload: err.response.data
+    )});
+};
+
+
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 };
