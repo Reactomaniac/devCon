a08f099f925f5f58b810f1df4174c9ec678bf697
Junaid Farooq <junaid@evercam.io>
2018-12-24 11:18:03 +0500

    Start deleting accounts.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from "./types";
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from "./types";
 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
@@ -29,6 +29,24 @@ export const createProfile = (profileData, history) => dispatch => {
         payload: err.response.data
       })
     );
+}
+
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      ).catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 }
 
 export const setProfileLoading = () => {
