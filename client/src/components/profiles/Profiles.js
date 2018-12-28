6f4e752f67147cf4a313f152cfe3b66f5b4493f7
Junaid Farooq <junaid@evercam.io>
2018-12-28 23:56:43 +0500

    Initiate Profiles components.

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Spinner from "../common/Spinner";
+import { getProfiles } from "../../actions/profileActions";
+
+class Profiles extends Component {
+
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        <h1>Profiles here</h1>
+      } else {
+        profileItems = <h4>No profiles found</h4>
+      }
+    }
+
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile
+})
+
+export default connect(null, { getProfiles })(Profiles);
