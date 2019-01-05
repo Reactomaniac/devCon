1198e8fcf608ba56a7e1478569208ea92905073d
Junaid Farooq <junaid@evercam.io>
2019-01-05 23:08:31 +0500

    Post Add (Semi working, there is an auth bug.)

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const  initiatState = {
+  posts: [],
+  posts: {},
+  loading: false
+};
+
+export default function(state = initiatState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
